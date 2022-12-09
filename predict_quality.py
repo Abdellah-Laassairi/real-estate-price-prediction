cba3fe9bf73fb2e96ba9ef2363caca368202ee5d
Abdellah-laassairi <laassairi.abdellah@gmail.com>
2022-12-09 19:11:01 +0100

    Added implementation of googles paper on image quality

---
+++ predict_quality.py
@@ -0,0 +1,18 @@
+import os
+
+test_dir = "data/reduced_images/test"
+train_dir = "data/reduced_images/train"
+
+test_images = [x[0] for x in os.walk(test_dir)]
+train_images = [x[0] for x in os.walk(train_dir)]
+
+
+for i in test_images[1:]:
+    cmd = f"image-quality-assessment/predict  \
+    --docker-image nima-cpu \
+    --base-model-name MobileNet \
+    --weights-file /home/odeck/Desktop/data-challenge-2022/image-quality-assessment/models/MobileNet/weights_mobilenet_technical_0.11.hdf5 \
+    --image-source /home/odeck/Desktop/data-challenge-2022/{i}"
+    os.system(cmd)
+    print(i)
+    print(cmd)
