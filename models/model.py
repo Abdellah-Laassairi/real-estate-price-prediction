2786a9cb0a538fcc3a75d8c82f828a635d56944c
alaassairi <abdellah.laassairi@telespazio.com>
2023-05-01 18:33:03 +0000

    update to model

--- models/model.py
+++ models/model.py
@@ -11,17 +11,19 @@ logger = TensorBoardLogger('tb_logs', name='my_model')
 
 
 def my_loss(output, target):
-    loss = torch.sqrt(torch.mean((output - target)**2))
+    mse = torch.nn.MSELoss()
+    loss = torch.sqrt(mse(output, target))
+    # loss = torch.sqrt(torch.mean((output - target) ** 2))
     return loss
 
 
 class RealModel(pl.LightningModule):
     def __init__(self,
-                 num_target_classes=int(4176),
+                 num_target_classes=int(400),
                  arch='efficientnet-b0',
                  lr=0.1,
                  weight_decay=0.001,
-                 batch_size=4,
+                 batch_size=8,
                  max_epochs=100,
                  *args,
                  **kwargs):
@@ -35,17 +37,27 @@ class RealModel(pl.LightningModule):
         self.max_epochs = max_epochs
         self.net = EfficientNet.from_pretrained(
             arch, advprop=True, num_classes=self.num_target_classes)
-        # Create loss function
 
-        self.linear = torch.nn.Linear(self.num_target_classes, 1, bias=True)
+        self.linear1 = torch.nn.Linear(self.num_target_classes, 1, bias=True)
+        self.linear1 = torch.nn.Linear(self.num_target_classes, 1, bias=True)
+
         self.relu = torch.nn.ReLU()
+        self.batchnorm = torch.nn.BatchNorm2d(
+            num_features=self.num_target_classes)
         self.loss_fn = my_loss
-        self.acc = R2Score()
+        self.acc = R2Score(num_outputs=self.batch_size,
+                           multioutput='raw_values')
+        self.dropout = torch.nn.Dropout(0.3)
 
     def forward(self, x):
         x1 = self.net(x)
-        x2 = self.linear(x1)
-        output = self.relu(x2).squeeze(1)
+        x2 = self.dropout(x1)
+        x3 = self.linear1(x2)
+        x4 = self.dropout(x3)
+        # x5 = self.relu(x4)
+        # x6 = self.linear(x5)
+        # x7 = self.dropout(x6)
+        output = self.relu(x4).squeeze(1)
         return output
 
     def configure_optimizers(self):
@@ -67,17 +79,23 @@ class RealModel(pl.LightningModule):
 
     def step(self, batch):
         x, y = batch
+        y = y.float()
         y_hat = self.forward(x)
+        # print("y", y.size())
+        # print("y_hat", y_hat.size())
         return y, y_hat
 
-    def training_step(self, batch, batch_nb):
+    def training_step(self, batch):
         # hardware agnostic training
         y, y_hat = self.step(batch)
 
         loss = self.loss_fn(y_hat, y)
 
-        self.log('Train_loss', loss, prog_bar=True)
-        self.train_output_list.append((y, y_hat))
+        self.log('Train_loss', loss, prog_bar=True, sync_dist=True)
+        self.train_output_list.append({
+            'y': y,
+            'y_hat': y_hat,
+        })
         return loss
 
     def on_train_epoch_end(self):
@@ -85,14 +103,15 @@ class RealModel(pl.LightningModule):
         targets = []
 
         for output in self.train_output_list:
-            preds.append(output[1])
-            targets.append(output[0])
+            if output['y_hat'].size()[0] == self.batch_size:
+                preds.append(output['y_hat'])
+                targets.append(output['y'])
 
         targets = torch.stack(targets)
         preds = torch.stack(preds)
-
         acc = self.acc(preds, targets)
-        self.log('R2_Train', acc)
+        self.log('R2_Train', acc.mean(), sync_dist=True)
+        print('R2_Train:', acc.mean())
         self.train_output_list.clear()
         return {
             'R2': acc,
@@ -100,10 +119,9 @@ class RealModel(pl.LightningModule):
 
     def validation_step(self, batch, batch_nb):
         y, y_hat = self.step(batch)
-        # acc = self.acc(y_hat, y )
         loss = self.loss_fn(y_hat, y)
         results = {'val_loss': loss, 'y': y.detach(), 'y_hat': y_hat.detach()}
-        self.log('Val_loss', loss, prog_bar=True)
+        self.log('Val_loss', loss, prog_bar=True, sync_dist=True)
 
         self.val_output_list.append(results)
         return results
@@ -113,14 +131,14 @@ class RealModel(pl.LightningModule):
         targets = []
 
         for output in self.val_output_list:
-            preds.append(output['y_hat'])
-            targets.append(output['y'])
-
+            if output['y_hat'].size()[0] == self.batch_size:
+                preds.append(output['y_hat'])
+                targets.append(output['y'])
         targets = torch.stack(targets)
         preds = torch.stack(preds)
-
         acc = self.acc(preds, targets)
-        self.log('R2_val', acc)
+        self.log('R2_val', acc.mean(), sync_dist=True)
+        print('R2_VAL:', acc.mean())
         self.val_output_list.clear()
         return {
             'R2': acc,
@@ -134,8 +152,8 @@ class RealModel(pl.LightningModule):
 
         limit_batches = self.trainer.limit_train_batches
         batches = len(self.trainer.train_dataloader())
-        batches = min(batches, limit_batches) if isinstance(
-            limit_batches, int) else int(limit_batches * batches)
+        batches = (min(batches, limit_batches) if isinstance(
+            limit_batches, int) else int(limit_batches * batches))
 
         num_devices = max(1, self.trainer.num_gpus, self.trainer.num_processes)
         if self.trainer.tpu_cores:
