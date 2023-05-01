71b97b0d80f33abb3f87eb4542a21b79dd951e3e
alaassairi <abdellah.laassairi@telespazio.com>
2023-05-01 18:33:30 +0000

    cleaned train

--- train.py
+++ train.py
@@ -4,22 +4,10 @@ from lightning.pytorch.cli import LightningCLI
 from models.model import *
 from pre.images import *
 
-TEST_SIZE = 0.2
-SEED = 42
-
 if __name__ == '__main__':
-
-    # model = RealModel(
-
-    # )
-    # trainer = pl.Trainer(fast_dev_run=5,
-    #                     devices=3,
-    #                     accelerator="gpu",
-    #                     train_dataloader=,
-    #                     val_dataloader=,
-    #                     )
-    # trainer.fit(model)
-    LightningCLI(model_class=RealModel,
-                 datamodule_class=RealDataModule,
-                 trainer_class=Trainer,
-                 run=True)
+    LightningCLI(
+        model_class=RealModel,
+        datamodule_class=RealDataModule,
+        trainer_class=Trainer,
+        run=True,
+    )
